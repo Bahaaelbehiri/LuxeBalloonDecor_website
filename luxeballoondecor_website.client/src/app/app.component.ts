27834c749eaf16ef60bf2da76e379a938b459fe5
GiftedR <kidsgamers@hotmail.com>
2024-05-06 13:57:52 -0700

    Removed broken code from "App.component.ts" that's created with default template

--- luxeballoondecor_website.client/src/app/app.component.ts
+++ luxeballoondecor_website.client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -5,23 +7,11 @@
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getForecasts();
-  }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
   }
 
   title = 'luxeballoondecor_website.client';
