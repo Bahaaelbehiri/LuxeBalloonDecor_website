ef9a6608ac74a48a1fb900ac12edd8bbe3c2388d
GiftedR <kidsgamers@hotmail.com>
2024-05-07 09:52:25 -0700

    Removed WeatherForecast Defaults, added Testimonial model

---
+++ LuxeBalloonDecor_website.Server/Models/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace LuxeBalloonDecor_website.Server.Models
+{
+    public class Testimonial
+    {
+        public int TestimonailID { get; set; }
+        public string TestimonialTitle { get; set; }
+        public DateOnly TestimonialDate { get; set; }
+        public string TestimonailDescription { get; set; }
+        public string TestimonailInitials { get; set; }
+    }
+}
