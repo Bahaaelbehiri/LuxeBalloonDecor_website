0c1988e98f28fd1c9de444d4e5bf423fc7316584
Bahaa <Bahaa@DESKTOP-FA8A547>
2024-05-16 09:45:40 -0700

    Add project files.

---
+++ luxeballoondecor_website.client/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsComponent } from './terms/terms.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }                                
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'terms', component: TermsComponent }
+  
+];
